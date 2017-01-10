4c661d0e730968197cfbc421a30425df6caf6fd1
Chanchal Raj <rajchanchalkhatri@gmail.com>
2017-01-10 11:20:21 +0400

    Update DenyPtrace.c
    the string "ptrace" is a dead giveaway, and should probably be obfuscated a bit. This commit contains the obfuscation code.

--- PtraceDemo/DenyPtrace.c
+++ PtraceDemo/DenyPtrace.c
@@ -7,9 +7,12 @@
 //
 
 #include <stdio.h>
+#import <string.h>
 #import <dlfcn.h>
 #import <sys/types.h>
-
+#define A(c)            (c) - 0x19
+#define UNHIDE_STR(str) do { char *p = str;  while (*p) *p++ += 0x19; } while (0)
+#define HIDE_STR(str)   do { char *p = str;  while (*p) *p++ -= 0x19; } while (0)
 typedef int (*ptrace_ptr_t)(int _request, pid_t _pid, caddr_t _addr, int _data);
 #if !defined(PT_DENY_ATTACH)
 #define PT_DENY_ATTACH 31
@@ -17,9 +20,15 @@ typedef int (*ptrace_ptr_t)(int _request, pid_t _pid, caddr_t _addr, int _data);
 
 void disable_gdb() {
     void* handle = dlopen(0, RTLD_GLOBAL | RTLD_NOW);
-    ptrace_ptr_t ptrace_ptr = dlsym(handle, "ptrace");
+    //Obsficate 'ptrace'string
+    char str[  ] = {
+        A('p'), A('t'), A('r'), A('a'), A('c'),
+        A('e'), 0
+    };
+    UNHIDE_STR(str);
+    char string[6];
+    strcpy(string, str);
+    ptrace_ptr_t ptrace_ptr = dlsym(handle, string);
     ptrace_ptr(PT_DENY_ATTACH, 0, 0, 0);
     dlclose(handle);
 }
-
-
